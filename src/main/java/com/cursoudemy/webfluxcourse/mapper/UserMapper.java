ceb2eef0a7284cd81b84d71b0c32eceb73cd162c
marwamprog-js <marwamprog.js@gmail.com>
2023-11-13 15:06:26 -0300

    add impl to update user contract

--- src/main/java/com/cursoudemy/webfluxcourse/mapper/UserMapper.java
+++ src/main/java/com/cursoudemy/webfluxcourse/mapper/UserMapper.java
@@ -3,10 +3,7 @@ package com.cursoudemy.webfluxcourse.mapper;
 import com.cursoudemy.webfluxcourse.entity.User;
 import com.cursoudemy.webfluxcourse.model.request.UserRequest;
 import com.cursoudemy.webfluxcourse.model.response.UserResponse;
-import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.NullValueCheckStrategy;
-import org.mapstruct.NullValuePropertyMappingStrategy;
+import org.mapstruct.*;
 
 @Mapper(
         componentModel = "spring",
@@ -17,6 +14,9 @@ public interface UserMapper {
 
     @Mapping(target = "id", ignore = true)
     User toEntity(final UserRequest request);
+
+    @Mapping(target = "id", ignore = true)
+    User toEntity(final UserRequest request, @MappingTarget final User entity);
 
     UserResponse toResponse(final User entity);
 
--- src/main/java/com/cursoudemy/webfluxcourse/service/UserService.java
+++ src/main/java/com/cursoudemy/webfluxcourse/service/UserService.java
@@ -34,4 +34,9 @@ public class UserService {
         return userRepository.findAll();
     }
 
+    public Mono<User> update(final String id, final UserRequest request) {
+        return findById(id).map(entity -> mapper.toEntity(request, entity))
+                .flatMap(userRepository::save);
+    }
+
 }
